9a876c6a9a5c1435171f436602e2d3fc37a9f2c5
Zhenya Adamchyk <zhenya.adamchyk@gmail.com>
2026-03-11 16:44:57 +0300

    refactor

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,7 +3,7 @@ import HomeView from "@/views/HomeView.vue";
 import RoomView from "@/views/RoomView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: "/",
